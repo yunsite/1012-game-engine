225c1ab6523db4d36f751c81f1f3de3cc0780403
decodeU@gmail.com <decodeU@gmail.com@578e9f8e-8753-808f-fd4f-26f585f54540>
2012-12-07 12:50:31 +0000

    数据库初始化完成

---
+++ trunk/1012.0.0.1/Smarty/templates_c/0013c647e9502d41f43f2055ab8406140c2510c6.test.conf.config.php
@@ -0,0 +1,16 @@
+<?php $_config_vars = array (
+  'sections' => 
+  array (
+    'setup' => 
+    array (
+      'vars' => 
+      array (
+        'bold' => true,
+      ),
+    ),
+  ),
+  'vars' => 
+  array (
+    'title' => '¹í±ø¶Ócutoff_size = 40',
+  ),
+); ?>
---
+++ trunk/1012.0.0.1/Test.php
@@ -0,0 +1,4 @@
+<?php
+include"GameControl.php";
+DataBaseInitialize();
+?>
