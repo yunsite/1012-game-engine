fe1200568e628c643c4ebfecf005a690a54d9c8a
decodeU@gmail.com <decodeU@gmail.com@578e9f8e-8753-808f-fd4f-26f585f54540>
2012-12-07 12:44:42 +0000

    移除以便测试上传

--- trunk/Connect.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-$con = mysql_connect("127.0.0.1","root","decode~U");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-mysql_query("set names 'UTF8'");
-?>
--- trunk/Test.php
+++
@@ -1,4 +0,0 @@
-<?php
-$xml = simplexml_load_file("GameData/Achievement.xml");
-echo $xml->Achievement[1]->Name;
-?>
